74da4fd2d6897d41934b24c57edb0293d8b87dc3
Michael Monaghan <Nostromos@users.noreply.github.com>
2023-08-07 10:12:16 -0400

    Searchbar labeling completed.

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,10 +3,12 @@ import React from 'react';
 function SearchBar() {
   return (
     <div className="search">
-      <label htmlFor="searchbar">
-        Search:
-        <input type="text" id="searchbar" />
-      </label>
+      <form>
+        <label htmlFor="searchbar">
+          Search:
+          <input type="text" id="searchbar" size="40" name="searchbar" placeholder="Search for your music..." />
+        </label>
+      </form>
     </div>
   );
 }
